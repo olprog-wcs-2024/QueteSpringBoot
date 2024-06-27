6fd0f3d4d4abb093f9fe52b24141d88fa27a0629
gaouimounir <mounir.gaoui@outlook.com>
2024-06-27 16:38:47 +0200

    feat: :zap: quete 1 spring boot fini

--- src/main/java/com/wildcodeschool/myProject/Controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/myProject/Controller/DoctorController.java
@@ -9,19 +9,19 @@ public class DoctorController {
 
     @GetMapping("/doctor/1")
     @ResponseBody
-    public String index() {
-        return "Greetings from Doctor 1!";
+    public String getDoctor1() {
+        return "William Hartnell";
     }
 
-    @GetMapping("/doctor/2")
+    @GetMapping("/doctor/10")
     @ResponseBody
-    public String index2() {
-        return "Greetings from Doctor 2!";
+    public String getDoctor10() {
+        return "David Tennant";
     }
 
-    @GetMapping("/doctor/3")
+    @GetMapping("/doctor/13")
     @ResponseBody
-    public String index3() {
-        return "Greetings from Doctor 3!";
+    public String getDoctor13() {
+        return "Jodie Whittaker";
     }
 }
