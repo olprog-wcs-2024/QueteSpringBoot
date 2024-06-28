59a871a02259c76cab6be32447688be75267ba5d
gaouimounir <mounir.gaoui@outlook.com>
2024-06-28 14:38:53 +0200

    feat: :zap: use responseEntity

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,31 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-@Controller
+@RestController
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-    @ResponseBody
-    public Doctor doctor() {
+    @GetMapping("/doctor/{number}")
 
-        return null;
+    public ResponseEntity<?> doctor(@PathVariable int number) {
+        if (number >= 1 && number <= 13) {
+            Doctor doctor13 = new Doctor(number, "Jodie Whittaker");
+            return ResponseEntity.ok(doctor13);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error", "Question Not Found"));
+
     }
+
 }
